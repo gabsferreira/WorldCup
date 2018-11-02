803c32b947a23094800cd857929ffddf45adfcd3
Gabriel <developer.gabriel@gmail.com>
2018-11-02 13:50:15 -0300

    Adding Models folder and Movie class.

---
+++ WorldCup.API/WorldCup.API/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCupAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; protected set; }
+        public string Title { get; protected set; }
+        public int Year { get; protected set; }
+        public double Rating { get; protected set; }
+
+        public Movie() { }
+
+        public Movie(int id, string title, int year, double rating)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Year = year;
+            this.Rating = rating;
+        }
+    }
+}
+
+
+
