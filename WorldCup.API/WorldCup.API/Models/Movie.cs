09027bebbc5eacef5401c1a7c662f9dbc219b427
Gabriel <developer.gabriel@gmail.com>
2018-11-02 14:11:59 -0300

    Actually we'll need a string for ID since it'll be populated from the server.

--- WorldCup.API/WorldCup.API/Models/Movie.cs
+++ WorldCup.API/WorldCup.API/Models/Movie.cs
@@ -7,14 +7,14 @@ namespace WorldCupAPI.Models
 {
     public class Movie
     {
-        public Guid Id { get; protected set; }
+        public string Id { get; protected set; }
         public string Title { get; protected set; }
         public int Year { get; protected set; }
         public double Rating { get; protected set; }
 
         public Movie() { }
 
-        public Movie(Guid id, string title, int year, double rating)
+        public Movie(string id, string title, int year, double rating)
         {
             this.Id = id;
             this.Title = title;
