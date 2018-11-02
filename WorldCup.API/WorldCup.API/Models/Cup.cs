e4af658da932e926f2a6f6e8162229c5139a40c7
Gabriel <developer.gabriel@gmail.com>
2018-11-02 14:37:16 -0300

    Adding Cup class.

---
+++ WorldCup.API/WorldCup.API/Models/Cup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldCupAPI.Models;
+
+namespace WorldCup.API.Models
+{
+    public class Cup
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<Movie> Filmes { get; }
+
+        public Cup(IEnumerable<Movie> filmes)
+        {
+            Id = new Guid();
+            this.Filmes = filmes;
+        }
+    }
+}
