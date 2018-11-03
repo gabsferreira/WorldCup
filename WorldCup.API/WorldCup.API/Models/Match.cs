5dabbd885002a644c57597e61cfb08619d4a241d
Gabriel <developer.gabriel@gmail.com>
2018-11-03 10:59:01 -0300

    Adding Macth class.

---
+++ WorldCup.API/WorldCup.API/Models/Match.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldCupAPI.Models;
+
+namespace WorldCup.API.Models
+{
+    public class Match
+    {
+        public Movie Winner { get; protected set; }
+        public Movie Loser { get; protected set; }
+        private IEnumerable<Movie> Players;
+
+        public Match(IEnumerable<Movie> players)
+        {
+            this.Players = players;
+        }
+
+        public void Play ()
+        {
+            this.Players.OrderByDescending(x => x.Rating)
+                .ThenBy(x => x.Title)
+                .ToList();
+
+            this.Winner = this.Players.First();
+            this.Loser = this.Players.Last();
+        }
+    }
+}
