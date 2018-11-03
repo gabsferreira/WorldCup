a437e03c99a4864ca8f1cad3cd7d8efef5ac9bea
Gabriel <developer.gabriel@gmail.com>
2018-11-03 16:09:11 -0300

    Adding MovieViewModel.

---
+++ WorldCup.API/WorldCup.API/ViewModels/MovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCup.API.ViewModels
+{
+    public class MovieViewModel
+    {
+        public string Id { get; set; }
+        public string Titulo { get; set; }
+        public string Ano { get; set; }
+        public string Nota { get; set; }
+    }
+}
