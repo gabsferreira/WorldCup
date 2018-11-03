4217587e5c9eea9f0febd4358b1ebc5912dd7172
Gabriel <developer.gabriel@gmail.com>
2018-11-03 11:25:31 -0300

    Adding Phase class.

---
+++ WorldCup.API/WorldCup.API/Models/Phase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCup.API.Models
+{
+    public abstract class Phase
+    {
+        private IEnumerable<Match> Matches;
+        public abstract void Run();
+    }
+}
