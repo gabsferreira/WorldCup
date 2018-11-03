d70792c6b8916c19d76e4af1af22ca848ef4dce1
Gabriel <developer.gabriel@gmail.com>
2018-11-03 11:36:21 -0300

    Adding phases to cup.

--- WorldCup.API/WorldCup.API/Models/Cup.cs
+++ WorldCup.API/WorldCup.API/Models/Cup.cs
@@ -10,6 +10,9 @@ namespace WorldCup.API.Models
     {
         public Guid Id { get; set; }
         public IReadOnlyList<Movie> Movies { get; set; }
+        public Phase FirstPhase { get; protected set; }
+        public Phase SemiFinal { get; protected set; }
+        public Phase Final { get; protected set; }
 
         public Cup(List<Movie> movies)
         {
@@ -20,6 +23,11 @@ namespace WorldCup.API.Models
         public void SortAlphabetically()
         {
             this.Movies = this.Movies.OrderBy(m => m.Title).ToList();
+        }
+
+        public void Run()
+        {
+            
         }
     }
 }
