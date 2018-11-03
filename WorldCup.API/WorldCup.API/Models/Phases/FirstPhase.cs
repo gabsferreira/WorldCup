c008632a8d64075aea8880b622e93d37ce14e1f9
Gabriel <developer.gabriel@gmail.com>
2018-11-03 12:34:55 -0300

    Adding logic to the first phase matchmaking.

--- WorldCup.API/WorldCup.API/Models/Phases/FirstPhase.cs
+++ WorldCup.API/WorldCup.API/Models/Phases/FirstPhase.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WorldCupAPI.Models;
 
 namespace WorldCup.API.Models.Phases
 {
     public class FirstPhase : Phase
     {
+        private const int numberOfMatches = 4;
+
+        public FirstPhase(IEnumerable<Movie> players)
+        {
+            MakeMatches(players);
+        }
+
+        private void MakeMatches(IEnumerable<Movie> players)
+        {
+            for (int i = 0; i < numberOfMatches ; i++)
+            {
+                var firstPlayer = players.Skip(i).Take(1).First();
+                var secondPlayer = players.Skip(players.Count() - i - 1).Take(1).First();
+
+                Match match = new Match(new List<Movie> { firstPlayer, secondPlayer });
+
+                this.Matches.Add(match);
+            }    
+        }
+
         public override void Run()
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
