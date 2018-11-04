eddc5e4cbfd0d5ebff38de099e4e6c9485936f91
Gabriel <developer.gabriel@gmail.com>
2018-11-04 20:11:15 -0200

    Adding MovieController.

---
+++ WorldCup.API/WorldCup.API/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorldCup.API.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
